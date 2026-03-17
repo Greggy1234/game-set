1bd9e6a2b448fd351d9d178b7c4c26de585bf0b6
Greggy1234 <gregoreid@googlemail.com>
2026-03-17 12:21:11 +0000

    Start setup of project and create home and tournament apps and models

---
+++ tournament/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Create your models here.
+class Tournament(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    series = models.CharField(max_length=100)
+    court = models.CharField(max_length=100)
+    surface = models.CharField(max_length=100)
+
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["name", "start_date"], name="unique_tournament")
+        ]
+  
+  
+    def __str__(self):
+        return f'{self.name} {self.start_date.year} ({self.series})'
+
+
+class Matches(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    best_of = models.IntegerField()
+    winner = models.CharField(max_length=100)
+    loser = models.CharField(max_length=100)
+    winner_rank = models.IntegerField()
+    loser_rank = models.IntegerField()
+    winner_set_1 = models.IntegerField(blank=True, null=True)
+    loser_set_1 = models.IntegerField(blank=True, null=True)
+    winner_set_2 = models.IntegerField(blank=True, null=True)
+    loser_set_2 = models.IntegerField(blank=True, null=True)
+    winner_set_3 = models.IntegerField(blank=True, null=True)
+    loser_set_3 = models.IntegerField(blank=True, null=True)
+    winner_set_4 = models.IntegerField(blank=True, null=True)
+    loser_set_4 = models.IntegerField(blank=True, null=True)
+    winner_set_5 = models.IntegerField(blank=True, null=True)
+    loser_set_5 = models.IntegerField(blank=True, null=True)
+    winner_sets = models.IntegerField(blank=True, null=True)
+    loser_sets = models.IntegerField(blank=True, null=True)
+    status = models.CharField(max_length=100)
+    draw_position = models.IntegerField(blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.winner} def. {self.loser}'
