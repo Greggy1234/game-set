43beac68405c1a10e3507af8c9eef7c57ba90269
Greggy1234 <gregoreid@googlemail.com>
2026-03-30 13:33:48 +0100

    Create models for product app and load all the data into the database

--- product/models.py
+++ product/models.py
@@ -15,17 +15,28 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    friendly_name = models.CharField(max_length=100, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    tag = models.ForeignKey('Tag', null=True, blank=True, on_delete=models.SET_NULL)
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
-    has_colours = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    image_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
