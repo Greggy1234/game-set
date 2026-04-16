123c04465a593cd5c1ec3b8b161c4a0b19220ead
Greggy1234 <gregoreid@googlemail.com>
2026-04-16 16:20:20 +0100

    Start styling templates for the Book Hub

--- book/urls.py
+++ book/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.book_home, name='book_hub'),
+    path("", views.book_overview, name='book_overview'),
     path("coaches/", views.coach_overview, name='coaches'),
-    path("court/<int:pk>/", views.court_detail, name='court_detail'),
+    path("court/<slug:slug>/", views.court_detail, name='court_detail'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Location
 
 # Create your views here.
-def book_home(request):
-    return "X"
+def book_overview(request):
+    locations = Location.objects.all()
+    
+    context = {
+        "locations": locations,
+    }
+    
+    return render(request, "book/book-overview.html", context)
 
 
 def court_detail(request, pk):
