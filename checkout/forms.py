2f2e6f13a5cfd5dccc9b639660853f4df82aa692
Greggy1234 <gregoreid@googlemail.com>
2026-04-19 21:08:14 +0100

    Create SHOP app checkout form

---
+++ checkout/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import ShopOrder
+
+
+class ShopOrderForm(forms.ModelForm):
+    class Meta:
+        model = ShopOrder
+        fields = ('full_name', 'email', 'phone_number', 'street_address1', 
+                  'street_address2', 'town_or_city', 'postcode', 'country', 'county',)
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders to SHOP app checkout form
+        """        
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County',
+        }
+        
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholder[field]
+            self.fields[field].widget.attrs["placeholder"]
+            self.fields[field].label = False
