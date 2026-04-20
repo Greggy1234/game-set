1e029c5fefcc03a355b35c4d0122e60487a80cb4
Greggy1234 <gregoreid@googlemail.com>
2026-04-20 18:34:45 +0100

    Create and test webhook handlers for Stripe

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path("shop/", views.shop_checkout, name='shop_checkout'),
     path("shop/checkout-success/<str:order_num>/", views.shop_checkout_success, name='shop_checkout_success'),
+    path("wh/", webhook, name="webhook"),
 ]
