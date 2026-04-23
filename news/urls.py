6196a3b32b9b2bb6660641ebbacce237d5ba102a
Greggy1234 <gregoreid@googlemail.com>
2026-04-23 10:20:29 +0100

    Create URL for comment deleteing on the NEWS hub articles

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.ArticleList.as_view(), name='news'),
     path("add-comment/<slug:slug>/", views.add_comment, name='add_comment'),
+    path("delete-comment/<int:comment_id>/", views.delete_comment, name='delete_comment'),
     path("<slug:slug>/", views.article_detail, name='article'),
 ]
