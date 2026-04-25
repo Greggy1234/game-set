621d42d2760aeeafa8d55d2c9afd35fa51385a24
Greggy1234 <gregoreid@googlemail.com>
2026-04-25 13:14:53 +0100

    Create models for BOOK hub bookings

--- checkout/models.py
+++ checkout/models.py
@@ -1,10 +1,10 @@
 import uuid
-
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 from django_countries.fields import CountryField
 from product.models import Product
+from book.models import Court, Coach
 from user_profile.models import Profile
 
 
@@ -77,3 +77,69 @@ class ShopOrderLineItem(models.Model):
 
     def __str__(self):
         return f'{self.product.name}: {self.product.sku} on order {self.order.order_number}'
+
+
+class BookingOrder(models.Model):
+    booking_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name="booking_orders")
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = CountryField(blank_label='GB', null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)    
+    original_bookings = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    
+    def _generate_booking_order_number(self):
+        """
+        Generate a random, unique order number
+        """
+        return uuid.uuid4().hex.upper()
+    
+    def update_bookings_total(self):
+        """
+        Update grand total each time a line item is added
+        """
+        self.grand_total = self.booking_lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.save()
+
+    def numb_of_bookings(self):
+        numb_of_bookings = self.booking_lineitems.objects.count()
+        return numb_of_bookings
+    
+    def save(self, *args, **kwargs):
+        """
+        Set order number if it hasn't been set yet
+        """
+        if not self.order_number:
+            self.order_number = self._generate_booking_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.order_number}: {self.grand_total}'
+
+
+class BookingOrderLineItem(models.Model):
+    booking = models.ForeignKey(BookingOrder, null=False, blank=False, on_delete=models.CASCADE, 
+                              related_name='booking_lineitems')
+    court = models.ForeignKey(Court, null=False, blank=False, on_delete=models.CASCADE, 
+                              related_name="court_lineitems")
+    coach = models.ForeignKey(Coach, null=True, blank=True, on_delete=models.CASCADE, 
+                              related_name="coach_lineitems")
+    date = models.DateField(null=False, blank=False)
+    time = models.TimeField(null=False, blank=False)
+    lineitem_total = models.IntegerField(null=False, blank=False, editable=False)
+    
+    def __str__(self):
+        coach_booked = "No coach booked"
+        if self.coach:
+            coach_booked = self.coach.name        
+        
+        return f'Booking for {self.court.name} on {self.date} at \
+            {self.time} with {coach_booked}. Order number: {self.booking.booking_number}'
--- checkout/signals.py
+++ checkout/signals.py
@@ -1,12 +1,12 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import ShopOrderLineItem
+from .models import ShopOrderLineItem, BookingOrderLineItem
 
 
 @receiver(post_save, sender=ShopOrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
-    Update order total on lineitem change or creation
+    Update order total on SHOP hub lineitem change or creation
     """
     instance.order.update_shop_total()
 
@@ -14,6 +14,21 @@ def update_on_save(sender, instance, created, **kwargs):
 @receiver(post_delete, sender=ShopOrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
     """
-    Update order total on lineitem deletion
+    Update order total on SHOP hub lineitem deletion
+    """
+    instance.order.update_shop_total()
+    
+    @receiver(post_save, sender=ShopOrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on SHOP hub lineitem change or creation
+    """
+    instance.order.update_shop_total()
+
+
+@receiver(post_delete, sender=ShopOrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update order total on SHOP hub lineitem deletion
     """
     instance.order.update_shop_total()
