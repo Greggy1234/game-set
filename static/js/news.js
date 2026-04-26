844cc421e4d955d0053e14d218c5b68f0a807557
Greggy1234 <gregoreid@googlemail.com>
2026-04-26 18:37:29 +0100

    Add JS to ensure the edit comment shows and traacks the information correctly

--- static/js/news.js
+++ static/js/news.js
@@ -2,5 +2,21 @@
 
 // Wait until DOM is loaded
 document.addEventListener("DOMContentLoaded", function () {
-    let editCommentButton = document.getElementById("")
+    let editCommentButton = document.getElementById("edit-comment-button");
+    editCommentButton.addEventListener("click", function () {
+        editComment();
+    })
 })
+
+/**
+ * This function lets users edit their comments ont he article.html page
+ */
+function editComment() {
+    const editContainerPreForm = document.getElementById("edit-comment-no-form-container");
+    const editFormContainer = document.getElementById("edit-comment-form-container");
+    const commentForm = document.getElementById("id_comment")
+    let userCommentOriginal = document.getElementById("user-comment").getAttribute("data-comment");
+    editContainerPreForm.classList.add("d-none");
+    editFormContainer.classList.remove("d-none");
+    commentForm.innerText = userCommentOriginal
+}
