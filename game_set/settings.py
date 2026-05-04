2674b5184fc8608c4bfb89208b2ba3c217c0df41
Greggy1234 <gregoreid@googlemail.com>
2026-05-04 16:48:38 +0100

    Update email settings

--- game_set/settings.py
+++ game_set/settings.py
@@ -221,19 +221,17 @@ STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET")
 
 # Email Configuration with Resend
-RESEND_API_KEY = os.environ.get('RESEND_API_KEY')
-
-if DEBUG:
+if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'game-set@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.resend.com'
-    EMAIL_PORT = 465
-    EMAIL_USE_SSL = True
-    EMAIL_HOST_USER = 'resend'
-    EMAIL_HOST_PASSWORD = RESEND_API_KEY
-    DEFAULT_FROM_EMAIL = 'Game-Sete@resend.dev'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
     ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
     EMAIL_SUBJECT_PREFIX = ''
 
